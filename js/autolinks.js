80c4adb8bca5e25432ce82a3cea41511a1e7c721
Matias Senger <m.senger@hotmail.com>
2022-11-09 16:55:06 +0100

    Updates

--- js/autolinks.js
+++ js/autolinks.js
@@ -2,6 +2,8 @@
 
 var labels_and_links = {
 	"Universität Zürich": "https://en.wikipedia.org/wiki/University_of_Zurich",
+	"UZH": "https://en.wikipedia.org/wiki/University_of_Zurich",
+	"PSI": "https://www.psi.ch/de",
 	"Universidad de Buenos Aires": "https://en.wikipedia.org/wiki/University_of_Buenos_Aires",
 	"CMS experiment": "https://cms.cern/",
 	"LHC": "https://home.cern/science/accelerators/large-hadron-collider",
@@ -9,6 +11,7 @@ var labels_and_links = {
 	"Fermilab": "https://www.fnal.gov/",
 	"UNSAM": "http://www.unsam.edu.ar/",
 	"skipper CCD": "https://arxiv.org/abs/1706.00028",
+	"CERN": "https://home.web.cern.ch/",
 }
 
 for (const [label, link] of Object.entries(labels_and_links))
