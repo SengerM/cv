168129bfcdef0e4bb028bd0ce57fbb407f0089d2
Matias Senger <m.senger@hotmail.com>
2023-08-20 15:41:29 +0200

    Some changes

--- make/main.py
+++ make/main.py
@@ -5,6 +5,7 @@ import json
 import datetime
 import humanize
 import pandas
+import logging
 
 BORDER_RADIUS = '0.5em'
 LIGHTER_TEXT_COLOR = '#878787'
@@ -22,7 +23,7 @@ def generate_personal_data():
 				style = f'display: flex; flex-direction: row; gap: 10px; padding: 10px; border-radius: {BORDER_RADIUS};',
 				cls = 'highlight_on_hover'
 			):
-				with tags.div(style='flex-shrink: 0; width: 1em;'):
+				with tags.div(style='flex-shrink: 0; width: 20px;'):
 					tags.img(
 						style = 'width: 100%;',
 						alt = data['field_name'], 
@@ -33,7 +34,7 @@ def generate_personal_data():
 					for s in data['content']:
 						if data['field_name']=='Email':
 							tags.a(s, href=f'mailto:{s}')
-						elif data['field_name'] in {'Website','LinkedIn'}:
+						elif data['field_name'] in {'Website','LinkedIn','GitHub profile'}:
 							tags.a(s, href=s)
 						else:
 							tags.div(s)
@@ -133,30 +134,37 @@ def generate_publications():
 				)
 				with tags.div(style='display: flex; flex-direction: column; gap: 1px;'):
 					tags.div(publication['title'], style='font-weight: bold;')
-					with tags.div():
-						tags.span(publication['journal'])
-						tags.span(',')
-						tags.span(publication['publisher'])
+					if all([_ in publication for _ in {'journal','publisher'}]):
+						with tags.div():
+							tags.span(publication['journal'])
+							tags.span('·')
+							tags.span(publication['publisher'])
+					else:
+						logging.warning(f'Publication {repr(publication["title"])} has no info for "journal" and or "publisher", will skip these fields for this particular publication.')
 					with tags.div(style=f'font-weight: 100; color: {LIGHTER_TEXT_COLOR}'):
 						tags.span(publication['publication_date'])
-					with tags.div(style=f'font-weight: 100; color: {LIGHTER_TEXT_COLOR}'):
-						tags.span('DOI: ')
-						tags.a(publication['DOI'], href=f'https://doi.org/{publication["DOI"]}')
+					if 'DOI' in publication:
+						with tags.div(style=f'font-weight: 100; color: {LIGHTER_TEXT_COLOR}'):
+							tags.span('DOI: ')
+							tags.a(publication['DOI'], href=f'https://doi.org/{publication["DOI"]}')
+					else:
+						logging.warning(f'Publication {repr(publication["title"])} has no info for "DOI", will skip this field for this particular publication.')
 	
+	tags.p('More publications in which I participated can be found in my academic profiles:')
 	with tags.div(style='display: flex; flex-direction: row; gap: 10px; padding: 10px;'):
 		tags.a('Google scholar', cls='button', href='https://scholar.google.com/citations?user=WDjszAYAAAAJ&hl')
 		tags.a('iNSPIRE', cls='button', href='https://inspirehep.net/literature?sort=mostrecent&size=25&page=1&q=find%20a%20matias%20senger')
 		tags.a('arXiv', cls='button', href='https://arxiv.org/search/?query=matias+senger&searchtype=author&abstracts=show&order=-announced_date_first&size=50')
 
+def generate_skill_bubble(skill):
+	n_stars = 1 if skill['Skillfulness (1-10)']>=8 else 0
+	tags.div(
+		skill['skill_name'] + (' '+'⭐'*n_stars)*(n_stars>0),
+		cls = 'skill_bubble',
+		title = skill['description'] if not isinstance(skill['description'], float) else None,
+	)
+
 def generate_skills():
-	def generate_skill_bubble(skill):
-		n_stars = 2 if skill['Skillfulness (1-10)']>=9 else 1 if skill['Skillfulness (1-10)']>=6 else 0
-		tags.div(
-			skill['skill_name'] + (' '+'⭐'*n_stars)*(n_stars>0),
-			cls = 'skill_bubble',
-			title = skill['description'] if not isinstance(skill['description'], float) else None,
-		)
-	
 	skills_data = pandas.read_csv(
 		'data/skills.csv',
 		dtype = {
@@ -182,30 +190,58 @@ def generate_skills():
 								generate_skill_bubble(skill)
 						else:
 							generate_skill_bubble(skill)
-						
 
-doc = dominate.document(title='Matias Senger curriculum vitae')
 
-with doc.head:
-	tags.link(rel='stylesheet', href='css/style.css')
-	tags.link(rel='preconnect', href='https://fonts.googleapis.com')
-	tags.link(rel='preconnect', href='https://fonts.gstatic.com', crossorigin=True)
-	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap')
-	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Source+Serif+4:opsz@8..60&display=swap')
-	tags.meta(name='viewport', content='width=device-width, initial-scale=1')
-
-with doc:
-	with tags.div(cls='sidenav'):
-		with tags.div(id='my_name_and_header_container'):
-			tags.div('Matias Senger', id='my_name')
-			tags.div('Physicist 🔭 and electronics engineer💡', style='color:#696969;')
-		generate_personal_data()
-	with tags.div(cls='main'):
-		generate_experience()
-		generate_education()
-		generate_publications()
-		generate_skills()
-
+if __name__ == '__main__':
+	import sys
 	
-with open('../test.html', 'w') as ofile:
-	print(doc, file=ofile)
+	logging.basicConfig(
+		stream = sys.stderr, 
+		level = logging.WARNING,
+		format = '%(asctime)s|%(levelname)s|%(funcName)s|%(message)s',
+		datefmt = '%Y-%m-%d %H:%M:%S',
+	)
+	
+	doc = dominate.document(title='Matias Senger curriculum vitae')
+
+	with doc.head:
+		tags.link(rel='stylesheet', href='css/style.css')
+		tags.link(rel='preconnect', href='https://fonts.googleapis.com')
+		tags.link(rel='preconnect', href='https://fonts.gstatic.com', crossorigin=True)
+		tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap')
+		tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Source+Serif+4:opsz@8..60&display=swap')
+		tags.meta(name='viewport', content='width=device-width, initial-scale=1')
+		tags.base(target="_blank") # All links open in new tab.
+
+	with doc:
+		with tags.div(cls='sidenav'):
+			with tags.div(id='my_name_and_header_container'):
+				tags.div('Matias Senger', id='my_name')
+				tags.div('Physicist 🔭 and electronics engineer💡', style='color:#696969;')
+			generate_personal_data()
+			
+			with tags.div(style='display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px; margin-top: 33px;'):
+				soft_skills_data = pandas.read_csv(
+					'data/skills.csv',
+					dtype = {
+						'skill_name': str,
+						'sub_skill_of': str,
+						'kind': str,
+						'category': str,
+						'Skillfulness (1-10)': int,
+						'description': str,
+						'url': str,
+					}
+				).query('kind=="soft"')
+				for _,skill in soft_skills_data.sort_values("Skillfulness (1-10)", ascending=False).iterrows():
+					generate_skill_bubble(skill)
+			
+		with tags.div(cls='main'):
+			generate_experience()
+			generate_education()
+			generate_publications()
+			generate_skills()
+
+		
+	with open('../test.html', 'w') as ofile:
+		print(doc, file=ofile)
