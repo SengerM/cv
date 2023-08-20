ea7580d76be3e2ebbc4877104f352e32b145128c
Matias Senger <m.senger@hotmail.com>
2023-08-20 10:45:29 +0200

    Better code

--- make/main.py
+++ make/main.py
@@ -8,25 +8,9 @@ import humanize
 BORDER_RADIUS = '0.5em'
 LIGHTER_TEXT_COLOR = '#878787'
 
-with open('data/personal.json') as ifile:
-	personal_data = json.load(ifile)
-
-doc = dominate.document(title='Matias Senger curriculum vitae')
-
-with doc.head:
-	tags.link(rel='stylesheet', href='css/style.css')
-	tags.link(rel='preconnect', href='https://fonts.googleapis.com')
-	tags.link(rel='preconnect', href='https://fonts.gstatic.com', crossorigin=True)
-	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap')
-	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Source+Serif+4:opsz@8..60&display=swap')
-	tags.meta(name='viewport', content='width=device-width, initial-scale=1')
-
-with doc:
-	with tags.div(id='sticky_header'):
-		tags.span('Matias Senger', id='my_name')
-	tags.p('Physicist 🔭 and electronics engineer💡', style='color:#696969;')
-	
-	# Personal data ---
+def generate_personal_data():
+	with open('data/personal.json') as ifile:
+		personal_data = json.load(ifile)
 	with tags.div(style='display: flex; flex-direction: column;'):
 		for data in personal_data:
 			with tags.div(
@@ -48,12 +32,14 @@ with doc:
 							tags.a(s, href=s)
 						else:
 							tags.div(s)
-	
-	tags.h1('Experience')
+
+def generate_experience():
 	with open('data/experience/experience.json') as ifile:
 		experience_data = json.load(ifile)
 	with open('data/experience/employers.json') as ifile:
 		employers_data = json.load(ifile)
+	
+	tags.h1('Experience')
 	with tags.div(style='display: flex; flex-direction: column;'):
 		for experience in experience_data:
 			with tags.div(
@@ -62,12 +48,13 @@ with doc:
 			):
 				employer_key = experience['employer']
 				with tags.div(style='flex-shrink: 0; width: 50px;'):
-					tags.img(
-						style = 'width: 100%;',
-						alt = employers_data[employer_key]['name'], 
-						title = employers_data[employer_key]['name'], 
-						**employers_data[employer_key]['logo'],
-					)
+					with tags.a(href=employers_data[employer_key]['website']):
+						tags.img(
+							style = 'width: 100%;',
+							alt = employers_data[employer_key]['name'], 
+							title = employers_data[employer_key]['name'], 
+							**employers_data[employer_key]['logo'],
+						)
 				with tags.div(style='display: flex; flex-direction: column; gap: 5px;'):
 					tags.div(experience['position_name'], style='font-weight: bolder;')
 					with tags.div():
@@ -91,12 +78,14 @@ with doc:
 						tags.span('·')
 						tags.span(experience['modality'])
 					tags.div(experience['description'])
-	
-	tags.h1('Education')
+
+def generate_education():
 	with open('data/education/education.json') as ifile:
 		education_data = json.load(ifile)
 	with open('data/education/schools.json') as ifile:
 		schools_data = json.load(ifile)
+	
+	tags.h1('Education')
 	with tags.div(style='display: flex; flex-direction: column;'):
 		for education in education_data:
 			with tags.div(
@@ -120,11 +109,14 @@ with doc:
 					with tags.div(style=f'font-weight: 100; color: {LIGHTER_TEXT_COLOR}'):
 						end_date = datetime.datetime.strptime(education['end_date'], '%b %Y')
 						tags.span(end_date.strftime('%b %Y') if end_date.date()!=datetime.datetime.today().date() else 'Present')
+
+def generate_publications():
+	with open('data/publications.json') as ifile:
+		publications_data = json.load(ifile)
 	
 	tags.h1('Publications')
 	tags.p('Publications with main participation:')
-	with open('data/publications.json') as ifile:
-		publications_data = json.load(ifile)
+	
 	with tags.div(style='display: flex; flex-direction: column;'):
 		for publication in publications_data:
 			if publication['title'] == '':
@@ -155,6 +147,28 @@ with doc:
 		tags.a('Google scholar', cls='button', href='https://scholar.google.com/citations?user=WDjszAYAAAAJ&hl')
 		tags.a('iNSPIRE', cls='button', href='https://inspirehep.net/literature?sort=mostrecent&size=25&page=1&q=find%20a%20matias%20senger')
 		tags.a('arXiv', cls='button', href='https://arxiv.org/search/?query=matias+senger&searchtype=author&abstracts=show&order=-announced_date_first&size=50')
+
+doc = dominate.document(title='Matias Senger curriculum vitae')
+
+with doc.head:
+	tags.link(rel='stylesheet', href='css/style.css')
+	tags.link(rel='preconnect', href='https://fonts.googleapis.com')
+	tags.link(rel='preconnect', href='https://fonts.gstatic.com', crossorigin=True)
+	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap')
+	tags.link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Source+Serif+4:opsz@8..60&display=swap')
+	tags.meta(name='viewport', content='width=device-width, initial-scale=1')
+
+with doc:
+	with tags.div(style='display: flex; flex-direction: row; height: 100%;'):
+		with tags.div():
+			with tags.div(style='rotate: -90deg; transform-origin: center; height: 333px;'):
+				tags.div('Matias Senger', style="font-size: 400%; font-family: 'Source Serif 4', serif; color: var(--headers-color);")
+				tags.div('Physicist 🔭 and electronics engineer💡', style='color:#696969;')
+			generate_personal_data()
+		with tags.div(style='overflow: auto;'):
+			generate_experience()
+			generate_education()
+			generate_publications()
 
 
 	
